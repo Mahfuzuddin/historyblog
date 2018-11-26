99233959430a39b26eae3cc2709579d604e60fe5
Jason <jasonalvarezcoraiza@gmail.com>
2018-11-26 18:19:00 -0500

    add css but its working

--- js/index.js
+++ js/index.js
@@ -12,11 +12,14 @@ databaseRef.once('value').then(function(snapshot){
     const databaseValues = snapshot.val();
     console.log(databaseValues);
     console.log(databaseValues["Facts"][0]["Fact"]);
-    for(var i=0; i<databaseValues["Facts"].length;i++){
+    //for(var i=0; i<databaseValues["Facts"].length;i++){
+    for(var key in databaseValues["Facts"]){
+
         console.log("hi");
         //$("#info").append(databaseValues["Facts"][i]["Fact"]);
-        $("#info").append("<div class='post'> <div id='user'> " + databaseValues["Users"][i]["Name"] + " </div> <div id='content'> " + databaseValues["Facts"][i]["Fact"] + " <div> </div>")
+        $("#info").append("<div class='post'> <div class='user'> " + databaseValues["Facts"][key]["Name"] + " </div> <div class='content'> " + databaseValues["Facts"][key]["Fact"] + " <div> </div>")
     }
+    
     
 
 });
@@ -24,6 +27,29 @@ databaseRef.once('value').then(function(snapshot){
 $( "#submit" ).click(function() {
     var input = $("#input").val();
     var name = $("#name").val();
-    $("#info").append("<div class='post'> "+input)+"</div>";
+    var period = $("#period").val();
+    var email = $("#email1").val();
+
+    var database = firebase.database();
+    var databaseRef = database.ref('/');
+    var FactsRef = database.ref('/Facts');
+    var UsersRef = database.ref('/Users');
+    databaseRef.once('value').then(function(snapshot){
+        const databaseValues = snapshot.val();
+        if( input != "" && name != "" && period != "" && email != ""){
+
+            FactsRef.push({
+                "Fact": input,
+                "Time Period": period,
+                "E-mail": email,
+                "Name": name,
+            });
+        } else {
+            $("#box").append("Please fill all fields");
+ 
+        }
+    })
+
+    $("#info").append("<div class='post'> <div class='user'> "+ name +" </div> <div class='content'>  "+input+" </div> </div>");
 
   });
